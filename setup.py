72108be9428ca96fb8caab3c941601900e9130c6
Umut Seven <umut-seven@cookpad.jp>
2021-01-20 12:31:56 +0000

    Set GFMDiagrams to lowercase

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="GFMDiagrams",
+    name="gfmdiagrams",
     version="0.2.0",
     url="https://github.com/cookpad/gfm-diagram",
     author="Ben Howes",
